7ccee996c4d9f46d849134df9e0a84c8116e3747
Rufat <huseynovrufat.usa@gmail.com>
2022-08-23 23:38:00 -0400

    class5 - update.Employee.java

--- src/first/Employee.java
+++ src/first/Employee.java
@@ -3,5 +3,6 @@ package first;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("SDET engineer");
+        System.out.println("Q/A engineer");
     }
 }
